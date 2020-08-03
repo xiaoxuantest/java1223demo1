1607d83d1dffa6213112e338671c9e7ead231af1
xiaoxuantest <47561914+xiaoxuantest@users.noreply.github.com>
2020-08-03 22:54:53 +0800

    Update 2.java

--- 2.java
+++ 2.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+//服务器端修改-------------------------^-^
 /**
  * 内容分类controller
  * @author Administrator
